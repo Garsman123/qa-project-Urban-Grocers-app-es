1dc8cf32cd9b53a4e1b8a6df01afaefddaf95125
Garsman123 <garsman@gmail.com>
2024-10-30 22:22:43 +0800

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = 'https://cnt-a7e9c49a-e177-4f93-82a0-12c60c753c18.containerhub.tripleten-services.com'
+URL_SERVICE = 'https://cnt-642bc483-1aa4-4776-85a4-e2bfc2fa8d9a.containerhub.tripleten-services.com'
 CREATE_USER_PATH = '/api/v1/users'
 KITS_PATH = '/api/v1/kits'
